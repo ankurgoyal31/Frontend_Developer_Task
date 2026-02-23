00c47cd7b5fae507a643ce2bca904bf9c720e91c
ankurgoyal31 <ankurgoyal1227@gmail.com>
2026-02-24 02:46:24 +0530

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,26 +3,38 @@ import React from 'react'
 import { useState } from 'react'
 import { login } from '../server/handle_data'
 import { useRouter } from 'next/navigation'
-const page = () => {
+import Link from 'next/link'
+ const page = () => {
     const router = useRouter();
   const [first, setfirst] = useState({email:'',password:''});
-  const submit = async()=>{
+  const[error,set_error] = useState(false)
+  const submit = async(e)=>{
+     if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(first.email)){
+alert("please enter valid email id");
+return
+     }
+if(first.name==""||first.email==""||first.password==""){
+      alert("please fill the require field....")
+      return
+    }
  let data = await login(first.email,first.password);
  if(data?.sucess){
 localStorage.setItem("user_token",data?.token)
 router.push("/");
-}
+}else{
+   set_error(true)
+ }
    }
   return (
-
       <>
-  <div className="login_wrapper">
+   <div className="login_wrapper">
     <div className="login_card">
+        {error && <><div>you have not registerd</div><div><Link className='register_btn_style' href={"/registration"}>register now</Link></div></>}
+
       <h2 className="login_title">Welcome Back</h2>
       <p className="login_sub">Login to continue</p>
-
-      <div className="input_group">
-        <input
+        <div className="input_group">
+         <input
           type="email"
           name="email"
           value={first.email}
@@ -41,15 +53,15 @@ router.push("/");
             setfirst({ ...first, [e.target.name]: e.target.value })
           }
           placeholder="Enter your password"
+          required
         />
       </div>
-
-      <button className="login_btn" onClick={submit}>
+       <button   className="login_btn" onClick={submit}>
         Login
       </button>
-    </div>
-  </div>
-</>
+       </div>
+   </div>
+ </>
 ) 
 }
 export default page
